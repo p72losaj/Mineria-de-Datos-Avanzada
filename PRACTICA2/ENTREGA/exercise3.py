f17abf2fd1cc5388b91374f0aba0effab966e948
p72losaj <p72losaj@uco.es>
2022-04-18 13:32:40 +0200

    Finalizacion ejercicio3 practica2

--- PRACTICA2/ENTREGA/exercise3.py
+++ PRACTICA2/ENTREGA/exercise3.py
@@ -6,12 +6,11 @@ from skmultilearn.problem_transform import LabelPowerset
 from sklearn.linear_model import LogisticRegression 
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier 
+from skmultilearn.adapt import MLkNN
+
 
 # classifiers list
-classifiers = ['Logistic Regression', 'SVC', 'DecisionTreeClassifier']
-lr=[]
-svc = []
-tree = []
+classifiers = ['Logistic Regression', 'SVC', 'DecisionTreeClassifier', 'MLKNN']
 # metrics list
 metric = ['Hamming Loss', 'Accuracy','f1_micro']
 
@@ -25,6 +24,7 @@ classifier = LabelPowerset(classifier=LogisticRegression(max_iter=10000), requir
 classifier.fit(X_train, y_train)
 prediction = classifier.predict(X_test)
 # Logistic Regression metrics
+lr=[]
 lr.append(metrics.hamming_loss(y_test, prediction))
 lr.append(metrics.accuracy_score(y_test, prediction))
 lr.append(metrics.f1_score(y_test,prediction,average='micro'))
@@ -34,6 +34,7 @@ classifier = LabelPowerset(classifier=SVC(), require_dense=[False, True])
 classifier.fit(X_train, y_train)
 prediction = classifier.predict(X_test)
 # SVC metrics
+svc=[]
 svc.append(metrics.hamming_loss(y_test, prediction))
 svc.append(metrics.accuracy_score(y_test, prediction))
 svc.append(metrics.f1_score(y_test,prediction,average='micro'))
@@ -43,14 +44,134 @@ classifier = LabelPowerset(classifier = DecisionTreeClassifier())
 classifier.fit(X_train,y_train)
 prediction = classifier.predict(X_test)
 # DecisionTreeClassifier metrics
+tree=[]
 tree.append(metrics.hamming_loss(y_test, prediction))
 tree.append(metrics.accuracy_score(y_test, prediction))
 tree.append(metrics.f1_score(y_test,prediction,average='micro'))
+# MLKNN classifier
+classifier = MLkNN(k=3)
+# Train and test
+classifier.fit(X_train, y_train)
+prediction = classifier.predict(X_test)
+# MLKNN metrics
+mlknn = []
+mlknn.append(metrics.hamming_loss(y_test, prediction))
+mlknn.append(metrics.accuracy_score(y_test, prediction))
+mlknn.append(metrics.f1_score(y_test,prediction,average='micro'))
+
 # Print the results
 for i in range (0,len(classifiers)): 
 	print('\t\t', classifiers[i], end = " ")
 print()
 
 for j in range (0,len(metric)):
-	print(metric[j], "\t", lr[j], "\t", svc[j], "\t", tree[j])	
+	print(metric[j], "\t", lr[j], "\t", svc[j], "\t", tree[j], "\t", mlknn[j])	
 
+## BIRDS DATASET ##
+X_train, y_train, feature_names, label_names = load_dataset('birds', 'train')
+X_test, y_test, _, _ = load_dataset('birds', 'test')
+
+# Method LabelPowerset with LogisticRegression
+classifier = LabelPowerset(classifier=LogisticRegression(max_iter=10000), require_dense=[False, True])
+# Train and test
+classifier.fit(X_train, y_train)
+prediction = classifier.predict(X_test)
+# Logistic Regression metrics
+lr=[]
+lr.append(metrics.hamming_loss(y_test, prediction))
+lr.append(metrics.accuracy_score(y_test, prediction))
+lr.append(metrics.f1_score(y_test,prediction,average='micro'))
+# Method LabelPowerset with SVC classifier
+classifier = LabelPowerset(classifier=SVC(), require_dense=[False, True])
+# Train and test
+classifier.fit(X_train, y_train)
+prediction = classifier.predict(X_test)
+# SVC metrics
+svc=[]
+svc.append(metrics.hamming_loss(y_test, prediction))
+svc.append(metrics.accuracy_score(y_test, prediction))
+svc.append(metrics.f1_score(y_test,prediction,average='micro'))
+# Method LabelPowerset with DecisionTreeClassifier
+classifier = LabelPowerset(classifier = DecisionTreeClassifier())
+# Train and test
+classifier.fit(X_train,y_train)
+prediction = classifier.predict(X_test)
+# DecisionTreeClassifier metrics
+tree=[]
+tree.append(metrics.hamming_loss(y_test, prediction))
+tree.append(metrics.accuracy_score(y_test, prediction))
+tree.append(metrics.f1_score(y_test,prediction,average='micro'))
+# MLKNN classifier
+classifier = MLkNN(k=3)
+# Train and test
+classifier.fit(X_train, y_train)
+prediction = classifier.predict(X_test)
+# MLKNN metrics
+mlknn = []
+mlknn.append(metrics.hamming_loss(y_test, prediction))
+mlknn.append(metrics.accuracy_score(y_test, prediction))
+mlknn.append(metrics.f1_score(y_test,prediction,average='micro'))
+
+# Print the results
+for i in range (0,len(classifiers)): 
+	print('\t\t', classifiers[i], end = " ")
+print()
+
+for j in range (0,len(metric)):
+	print(metric[j], "\t", lr[j], "\t", svc[j], "\t", tree[j], "\t", mlknn[j])	
+	
+## DATASET SCENE ##
+X_train, y_train, feature_names, label_names = load_dataset('scene', 'train')
+X_test, y_test, _, _ = load_dataset('scene', 'test')
+
+# Method LabelPowerset with LogisticRegression
+classifier = LabelPowerset(classifier=LogisticRegression(max_iter=10000), require_dense=[False, True])
+# Train and test
+classifier.fit(X_train, y_train)
+prediction = classifier.predict(X_test)
+# Logistic Regression metrics
+lr=[]
+lr.append(metrics.hamming_loss(y_test, prediction))
+lr.append(metrics.accuracy_score(y_test, prediction))
+lr.append(metrics.f1_score(y_test,prediction,average='micro'))
+# Method LabelPowerset with SVC classifier
+classifier = LabelPowerset(classifier=SVC(), require_dense=[False, True])
+# Train and test
+classifier.fit(X_train, y_train)
+prediction = classifier.predict(X_test)
+# SVC metrics
+svc=[]
+svc.append(metrics.hamming_loss(y_test, prediction))
+svc.append(metrics.accuracy_score(y_test, prediction))
+svc.append(metrics.f1_score(y_test,prediction,average='micro'))
+# Method LabelPowerset with DecisionTreeClassifier
+classifier = LabelPowerset(classifier = DecisionTreeClassifier())
+# Train and test
+classifier.fit(X_train,y_train)
+prediction = classifier.predict(X_test)
+# DecisionTreeClassifier metrics
+tree=[]
+tree.append(metrics.hamming_loss(y_test, prediction))
+tree.append(metrics.accuracy_score(y_test, prediction))
+tree.append(metrics.f1_score(y_test,prediction,average='micro'))
+# MLKNN classifier
+classifier = MLkNN(k=3)
+# Train and test
+classifier.fit(X_train, y_train)
+prediction = classifier.predict(X_test)
+# MLKNN metrics
+mlknn = []
+mlknn.append(metrics.hamming_loss(y_test, prediction))
+mlknn.append(metrics.accuracy_score(y_test, prediction))
+mlknn.append(metrics.f1_score(y_test,prediction,average='micro'))
+
+# Print the results
+for i in range (0,len(classifiers)): 
+	print('\t\t', classifiers[i], end = " ")
+print()
+
+for j in range (0,len(metric)):
+	print(metric[j], "\t", lr[j], "\t", svc[j], "\t", tree[j], "\t", mlknn[j])	
+	
+
+print("END")
--- PRACTICA2/ENTREGA/graphics.py
+++ PRACTICA2/ENTREGA/graphics.py
@@ -3,58 +3,71 @@
 import matplotlib.pyplot as plt
 
 ### TRAIN METRICS ####
-# Birds dataset train: 322
-# Emotions dataset train: 391
-# Scene dataset train: 1211
 train = [322,391,1211]
 # HammingLoss metrics
 lr_hammingLoss_train = [0.061,0.217,0.104]
 svc_hammingLoss_train = [0.053,0.325,0.084]
 tree_hammingLoss_train = [0.063,0.283,0.159]
-lr_hammingLoss_lp_train = [,]
-svc_hammingLoss_lp_train = [,]
-tree_hammingLoss_lp_train = [,]
+
+lr_hammingLoss_lp_train = [0.062,0.220,0.095]
+svc_hammingLoss_lp_train = [0.056,0.450,0.086]
+tree_hammingLoss_lp_train = [0.069,0.290, 0.152]
+mlknn_hammingLoss_train = [0.062,0.295, 0.102]
+
 # Accuracy metrics
 lr_accuracy_train = [0.464,0.257,0.523]
 svc_accuracy_train = [0.471, 0.015, 0.587]
 tree_accuracy_train = [0.406, 0.139, 0.352]
-lr_accuracy_lp_train = [,]
-svc_accuracy_lp_train = [,]
-tree_accuracy_lp_train = [,]
+
+lr_accuracy_lp_train = [0.464,0.332, 0.681]
+svc_accuracy_lp_train = [0.477,0.129, 0.708]
+tree_accuracy_lp_train = [0.461,0.208, 0.517]
+mlknn_accuracy_train = [0.452,0.193, 0.558]
 # F1 micro metrics train
 lr_micro_train = [0.381, 0.631,0.682]
 svc_micro_train = [0.018, 0.075, 0.725]
 tree_micro_train = [0.410, 0.564, 0.574]
-lr_micro_lp_train = [,]
-svc_micro_lp_train = [,]
-tree_micro__lp_train = [,]
 
+lr_micro_lp_train = [0.354,0.672, 0.729]
+svc_micro_lp_train = [0.099,0.270,0.753]
+tree_micro_lp_train = [0.294,0.559, 0.575]
+mlknn_micro_train = [0.132,0.557, 0.685]
+	 		
 ## TESTS METRICS ###
-# Emotions dataset test:  202
-# Birds dataset test:  323
-# Scene dataset test:  1196 
+
 tests = [202,323,1196]
 # Hamming Loss metrics
 lr_hammingLoss_test = [0.217,0.061,0.104]
 svc_hammingLoss_test = [0.325,0.053,0.084]
 tree_hammingLoss_test = [0.283,0.063,0.159]
-lr_hammingLoss_lp_test = []
-svc_hammingLoss_lp_test = []
-tree_hammingLoss_lp_test = []
+
+lr_hammingLoss_lp_test = [0.220,0.062,0.095]
+svc_hammingLoss_lp_test = [0.450,0.056,0.086]
+tree_hammingLoss_lp_test = [0.290,0.069, 0.152]
+mlknn_hammingLoss_test = [0.295, 0.062, 0.102]
+
 # Accuracy metrics
 lr_accuracy_test = [0.257,0.464,0.523]
 svc_accuracy_test = [0.015,0.471,0.587]
 tree_hammingLoss_test = [0.139,0.406,0.352]
-lr_accuracy_lp_test = []
-svc_accuracy_lp_test = []
-tree_hammingLoss_lp_test = []
+
+lr_accuracy_lp_test = [0.332,0.464, 0.681]
+svc_accuracy_lp_test = [0.129,0.477, 0.708]
+tree_hammingLoss_lp_test = [0.208,0.461, 0.517]
+mlknn_hammingLoss_test = [0.193,0.452, 0.558]
+
 # F1 micro metrics
 lr_micro_test = [0.631,0.381,0.682]
 svc_micro_test = [0.075,0.019,0.725]
 tree_micro_test = [0.564,0.410,0.574]
-lr_micro_lp_test = []
-svc_micro_lp_test = []
-tree_micro_test = []
+
+lr_micro_lp_test = [0.672,0.354, 0.729]
+svc_micro_lp_test = [0.270,0.099, 0.753]
+tree_micro_test = [0.559,0.294, 0.575]
+mlknn_micro_test = [0.516,0.132, 0.685]
+
+
+
 
 # Hamming Loss exercise2
 #plt.plot(nElements, lr_hammingLoss, label = "Logistic Regression")

0871d52a3dbbbeeb88f60ac9479b7917fb270b05
p72losaj <p72losaj@uco.es>
2022-04-18 14:01:14 +0200

    Generacion graficas ejercicio 5 practica 2

--- PRACTICA2/ENTREGA/graphics.py
+++ PRACTICA2/ENTREGA/graphics.py
@@ -35,8 +35,17 @@ mlknn_micro_train = [0.132,0.557, 0.685]
 
 # Graphics of train
 
-plt.plot(train,lr_hammingLoss_train,label='Logistic Regression'); plt.xlabel('Number of trains');plt.ylabel('Hamming Loss');plt.title('Train hamming loss multi-label problems'); plt.legend();plt.show()
+# plt.plot(train,lr_hammingLoss_train,label='Logistic Regression');plt.plot(train,svc_hammingLoss_train,label='SVC'); plt.plot(train,tree_hammingLoss_train,label='DecissionTreeClassifier'); plt.plot(train,mlknn_hammingLoss_train,label="mlknn"); plt.xlabel('Number of trains');plt.ylabel('Hamming Loss');plt.title('Train hamming loss multi-label problems'); plt.legend();plt.show()
 
+# plt.plot(train,lr_hammingLoss_lp_train,label='Logistic Regression');plt.plot(train,svc_hammingLoss_lp_train,label='SVC'); plt.plot(train,tree_hammingLoss_lp_train,label='DecissionTreeClassifier'); plt.plot(train,mlknn_hammingLoss_train,label="mlknn"); plt.xlabel('Number of trains');plt.ylabel('Hamming Loss');plt.title('Train hamming loss multi-label problems with method LP'); plt.legend();plt.show()
+
+# plt.plot(train,lr_accuracy_train,label='Logistic Regression');plt.plot(train,svc_accuracy_train,label='SVC'); plt.plot(train,tree_accuracy_train,label='DecissionTreeClassifier'); plt.plot(train,mlknn_accuracy_train,label="mlknn"); plt.xlabel('Number of trains');plt.ylabel('Accuracy');plt.title('Train accuracy multi-label problems'); plt.legend();plt.show()
+
+# plt.plot(train,lr_accuracy_lp_train,label='Logistic Regression');plt.plot(train,svc_accuracy_lp_train,label='SVC'); plt.plot(train,tree_accuracy_lp_train,label='DecissionTreeClassifier'); plt.plot(train,mlknn_accuracy_train,label="mlknn"); plt.xlabel('Number of trains');plt.ylabel('Accuracy');plt.title('Train accuracy multi-label problems with method LP'); plt.legend();plt.show()
+	 		
+# plt.plot(train,lr_micro_train,label='Logistic Regression');plt.plot(train,svc_micro_train,label='SVC'); plt.plot(train,tree_micro_train,label='DecissionTreeClassifier'); plt.plot(train,mlknn_micro_train,label="mlknn"); plt.xlabel('Number of trains');plt.ylabel('F1 micro');plt.title('Train f1 micro multi-label problems'); plt.legend();plt.show()
+
+# plt.plot(train,lr_micro_lp_train,label='Logistic Regression');plt.plot(train,svc_micro_lp_train,label='SVC'); plt.plot(train,tree_micro_lp_train,label='DecissionTreeClassifier'); plt.plot(train,mlknn_micro_train,label="mlknn"); plt.xlabel('Number of trains');plt.ylabel('F1 micro');plt.title('Train f1 micro multi-label problems with method LP'); plt.legend();plt.show()	 		
 	 		
 ## TESTS METRICS ###
 
@@ -54,12 +63,12 @@ mlknn_hammingLoss_test = [0.295, 0.062, 0.102]
 # Accuracy metrics
 lr_accuracy_test = [0.257,0.464,0.523]
 svc_accuracy_test = [0.015,0.471,0.587]
-tree_hammingLoss_test = [0.139,0.406,0.352]
+tree_accuracy_test = [0.139,0.406,0.352]
 
 lr_accuracy_lp_test = [0.332,0.464, 0.681]
 svc_accuracy_lp_test = [0.129,0.477, 0.708]
-tree_hammingLoss_lp_test = [0.208,0.461, 0.517]
-mlknn_hammingLoss_test = [0.193,0.452, 0.558]
+tree_accuracy_lp_test = [0.208,0.461, 0.517]
+mlknn_accuracy_test = [0.193,0.452, 0.558]
 
 # F1 micro metrics
 lr_micro_test = [0.631,0.381,0.682]
@@ -68,35 +77,19 @@ tree_micro_test = [0.564,0.410,0.574]
 
 lr_micro_lp_test = [0.672,0.354, 0.729]
 svc_micro_lp_test = [0.270,0.099, 0.753]
-tree_micro_test = [0.559,0.294, 0.575]
+tree_micro_lp_test = [0.559,0.294, 0.575]
 mlknn_micro_test = [0.516,0.132, 0.685]
 
 # Graphics of test
 
-# Hamming Loss exercise2
-#plt.plot(nElements, lr_hammingLoss, label = "Logistic Regression")
-#plt.plot(nElements, svc_hammingLoss, label = "SVC")
-#plt.plot(nElements, tree_hammingLoss, label = "DecisionTreeClassifier")
-#plt.xlabel('Number of elements of dataset');plt.ylabel('Hamming Loss');
-#plt.title('hamming loss multi-label problems')
-#plt.legend()
-#plt.show()
-# Accuracy scene exercise 2
-#plt.plot(nElements, lr_accuracyScene, label = "Logistic Regression")
-#plt.plot(nElements, svc_accuracyScene, label = "SVC")
-#plt.plot(nElements, tree_accuracyScene, label = "DecisionTreeClassifier")
-#plt.xlabel('Number of elements of dataset');plt.ylabel('Accuracy Scene');
-#plt.title('Accuracy Scene multi-label problems')
-#plt.legend()
-#plt.show()
+# plt.plot(tests,lr_hammingLoss_test,label='Logistic Regression');plt.plot(tests,svc_hammingLoss_test,label='SVC'); plt.plot(tests,tree_hammingLoss_test,label='DecissionTreeClassifier'); plt.plot(tests,mlknn_hammingLoss_test,label="mlknn"); plt.xlabel('Number of tests');plt.ylabel('Hamming Loss');plt.title('Tests hamming loss multi-label problems'); plt.legend();plt.show()
 
-# f1_micro exercise2
-#plt.plot(nElements, lr_f1Micro, label = "Logistic Regression")
-#plt.plot(nElements, svc_f1Micro, label = "SVC")
-#plt.plot(nElements, tree_f1Micro, label = "DecisionTreeClassifier")
-#plt.xlabel('Number of elements of dataset');plt.ylabel('f1_Micro');
-#plt.title('F1 Micro multi-label problemas')
-#plt.legend()
-#plt.show()
+# plt.plot(tests,lr_hammingLoss_lp_test,label='Logistic Regression');plt.plot(tests,svc_hammingLoss_lp_test,label='SVC'); plt.plot(tests,tree_hammingLoss_lp_test,label='DecissionTreeClassifier'); plt.plot(tests,mlknn_hammingLoss_test,label="mlknn"); plt.xlabel('Number of tests');plt.ylabel('Hamming Loss');plt.title('Test hamming loss multi-label problems with method LP'); plt.legend();plt.show()
 
+# plt.plot(tests,lr_accuracy_test,label='Logistic Regression');plt.plot(tests,svc_accuracy_test,label='SVC'); plt.plot(tests,tree_accuracy_test,label='DecissionTreeClassifier'); plt.plot(tests,mlknn_accuracy_test,label="mlknn"); plt.xlabel('Number of tests');plt.ylabel('Accuracy');plt.title('Tests accuracy multi-label problems'); plt.legend();plt.show()
 
+# plt.plot(tests,lr_accuracy_lp_test,label='Logistic Regression');plt.plot(tests,svc_accuracy_lp_test,label='SVC'); plt.plot(tests,tree_accuracy_lp_test,label='DecissionTreeClassifier'); plt.plot(tests,mlknn_accuracy_test,label="mlknn"); plt.xlabel('Number of tests');plt.ylabel('Accuracy');plt.title('Tests accuracy multi-label problems with method LP'); plt.legend();plt.show()
+	 		
+# plt.plot(tests,lr_micro_test,label='Logistic Regression');plt.plot(tests,svc_micro_test,label='SVC'); plt.plot(tests,tree_micro_test,label='DecissionTreeClassifier'); plt.plot(tests,mlknn_micro_test,label="mlknn"); plt.xlabel('Number of tests');plt.ylabel('F1 micro');plt.title('Tests f1 micro multi-label problems'); plt.legend();plt.show()
+
+plt.plot(tests,lr_micro_lp_test,label='Logistic Regression');plt.plot(tests,svc_micro_lp_test,label='SVC'); plt.plot(tests,tree_micro_lp_test,label='DecissionTreeClassifier'); plt.plot(tests,mlknn_micro_test,label="mlknn"); plt.xlabel('Number of tests');plt.ylabel('F1 micro');plt.title('Test f1 micro multi-label problems with method LP'); plt.legend();plt.show()	 		
